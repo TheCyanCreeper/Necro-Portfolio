670a645a77125504f5e11f95ae92bdc16d97dba8
shawnjamin <shawnallen1@mail.weber.edu>
2025-10-20 23:19:05 -0600

    Another

--- netlify/functions/app.ts
+++ netlify/functions/app.ts
@@ -1,15 +1,27 @@
+// import express, { Router } from "express";
+// import serverless from "serverless-http";
+
+// const api = express();
+
+// const router = Router();
+
+// app.use(express.static(__dirname + '/public'))
+
+// router.get('/', (req,res) => {
+//   res.sendFile(path.join(__dirname + '/index.html'));
+// });
+
+// api.use("/api/", router);
+
+// export const handler = serverless(api);
+
 import express, { Router } from "express";
 import serverless from "serverless-http";
 
 const api = express();
 
 const router = Router();
-
-app.use(express.static(__dirname + '/public'))
-
-router.get('/', (req,res) => {
-  res.sendFile(path.join(__dirname + '/index.html'));
-});
+router.get("/hello", (req, res) => res.send("Hello World!"));
 
 api.use("/api/", router);
 
